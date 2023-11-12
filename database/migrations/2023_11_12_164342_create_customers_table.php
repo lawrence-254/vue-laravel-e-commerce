6337d5b7a64e149ffa819fd8b493ea384896255c
lawrence-254 <lodhiambo26@gmail.com>
2023-11-12 21:52:30 +0300

    customer addresses

--- database/migrations/2023_11_12_164342_create_customers_table.php
+++ database/migrations/2023_11_12_164342_create_customers_table.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string(column:'status',length:45)->nullable();
             $table->foreignIdFor(model: User::class, column:'created_by')->nullable();
             $table->timestamps();
+            $table->foreignIdFor(model: User::class, column:'updated_by')->nullable();
         });
     }
 
--- database/migrations/2023_11_12_164349_create_customer_addresses_table.php
+++ database/migrations/2023_11_12_164349_create_customer_addresses_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('customer_addresses', function (Blueprint $table) {
             $table->id();
+            $table->string(column:'type',length:50);
+            $table->string(column:'address_1',length:255);
+            $table->string(column:'address_2',length:255);
+            $table->string(column:'city',length:255);
+            $table->string(column:'state',length:255)->nullable();
+            $table->string(column:'zip_code',length:255);
+            $table->string(column:'country_code',length:255);
+            $table->foreignId(column:'customer_id')->references(column:'id')->on(table:'customers');
             $table->timestamps();
+            $table->foreignId(column:'country_code')->references(column:'code')->on(table:'countries');
+
         });
     }
 
