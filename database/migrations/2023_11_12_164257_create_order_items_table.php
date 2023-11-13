60a3284e76abd7881eb66c00f762a089d4ab2e22
lawrence-254 <lodhiambo26@gmail.com>
2023-11-13 08:39:56 +0300

    migration configurations

--- database/migrations/2023_11_12_164257_create_order_items_table.php
+++ database/migrations/2023_11_12_164257_create_order_items_table.php
@@ -4,20 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateOrderItemsTable extends Migration
 {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
+        Schema::dropIfExists('order_items');
+
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
-            $table->foreignId(column:'order_id')->references(column:'id')->on(table:'orders');
-            $table->foreignId(column:'product_id')->references(column:'id')->on(table:'products');
-            $table->integer(column:'quantity');
-            $table->decimal(column:'unit_price');
+            $table->integer('quantity');
+            $table->decimal('unit_price', 8, 2);
             $table->timestamps();
+
+            // Define foreign key relationships
+            $table->foreignId('order_id')->constrained('orders');
+            $table->foreignId('product_id')->constrained('products');
         });
     }
 
@@ -28,4 +32,40 @@ return new class extends Migration
     {
         Schema::dropIfExists('order_items');
     }
-};
+}
+
+
+
+
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
+
+// return new class extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      */
+//     public function up(): void
+//     {
+//         Schema::dropIfExists('order_items');
+
+//         Schema::create('order_items', function (Blueprint $table) {
+//             $table->id();
+//             $table->integer(column:'quantity');
+//             $table->decimal(column:'unit_price');
+//             $table->timestamps();
+//             $table->foreignId(column:'order_id')->references(column:'id')->on(table:'orders');
+//             $table->foreignId(column:'product_id')->references(column:'id')->on(table:'products');
+
+//         });
+//     }
+
+//     /**
+//      * Reverse the migrations.
+//      */
+//     public function down(): void
+//     {
+//         Schema::dropIfExists('order_items');
+//     }
+// }; -->
--- database/migrations/2023_11_12_164349_create_customer_addresses_table.php
+++ database/migrations/2023_11_12_164349_create_customer_addresses_table.php
@@ -9,23 +9,42 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+        public function up(): void
     {
+        Schema::dropIfExists('customer_addresses');
+
         Schema::create('customer_addresses', function (Blueprint $table) {
             $table->id();
-            $table->string(column:'type',length:50);
-            $table->string(column:'address_1',length:255);
-            $table->string(column:'address_2',length:255);
-            $table->string(column:'city',length:255);
-            $table->string(column:'state',length:255)->nullable();
-            $table->string(column:'zip_code',length:255);
-            $table->string(column:'country_code',length:255);
-            $table->foreignId(column:'customer_id')->references(column:'id')->on(table:'customers');
+            $table->string('type', 50);
+            $table->string('address_1', 255);
+            $table->string('address_2', 255);
+            $table->string('city', 255);
+            $table->string('state', 255)->nullable();
+            $table->string('zip_code', 255);
+            $table->string('country_code', 255);
+            $table->foreignId('customer_id')->references('id')->on('customers');
             $table->timestamps();
-            $table->foreignId(column:'country_code')->references(column:'code')->on(table:'countries');
 
+            $table->foreign('country_code')->references('code')->on('countries');
         });
     }
+    // public function up(): void
+    // {
+    //     Schema::create('customer_addresses', function (Blueprint $table) {
+    //         $table->id();
+    //         $table->string(column:'type',length:50);
+    //         $table->string(column:'address_1',length:255);
+    //         $table->string(column:'address_2',length:255);
+    //         $table->string(column:'city',length:255);
+    //         $table->string(column:'state',length:255)->nullable();
+    //         $table->string(column:'zip_code',length:255);
+    //         $table->string(column:'country_code',length:255);
+    //         $table->foreignId(column:'customer_id')->references(column:'id')->on(table:'customers');
+    //         $table->timestamps();
+    //         $table->foreignId(column:'country_code')->references(column:'code')->on(table:'countries');
+
+    //     });
+    // }
 
     /**
      * Reverse the migrations.
@@ -35,3 +54,4 @@ return new class extends Migration
         Schema::dropIfExists('customer_addresses');
     }
 };
+ 
