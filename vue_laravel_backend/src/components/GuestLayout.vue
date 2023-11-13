444524f9b788ecbdb639fc52e0223fadd5fd95c1
lawrence-254 <lodhiambo26@gmail.com>
2023-11-13 20:24:43 +0300

    forms

--- vue_laravel_backend/src/components/GuestLayout.vue
+++ vue_laravel_backend/src/components/GuestLayout.vue
@@ -1,8 +1,33 @@
 <template>
 
+    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img class="mx-auto h-10 w-auto" src=""
+                alt="Your Company" />
+            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+               {{ title }}
+            </h2>
+        </div>
 
-<GuestLayout title="Sign in into your account">
-            <!-- <form class="space-y-6" action="#" method="POST"> -->
+<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+<form class="space-y-6" action="#" method="POST">
+     <slot></slot>
+</form>
+
+</div>
+</div>
+</template>
+<script setup>
+const {title} = defineProps({
+    title: String
+
+})
+</script>
+
+<style>
+
+</style>
+<!--             <form class="space-y-6" action="#" method="POST">
                 <div>
                     <label class="block text-sm font-medium leading-6 text-gray-900">Login</label>
                     <div class="mt-2">
@@ -34,20 +59,4 @@
                         class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
                         in</button>
                 </div>
-            <!-- </form> -->
-    </GuestLayout>
-            <p class="mt-10 text-center text-sm text-gray-500">
-                Not a member?
-                {{ ' ' }}
-                <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Sign Up</a>
-        </p>
-</template>
-
-<script setup>
-import GuestLayout from '../components/GuestLayout.vue';
-</script>
-<style scoped>
-/* Your scoped styles here */
-</style>
-
-
+            </form> -->
