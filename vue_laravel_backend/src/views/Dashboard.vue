19734143fdeaa50266f37bdb764e5923d6ee63e9
lawrence-254 <lodhiambo26@gmail.com>
2023-11-13 22:25:53 +0300

    setup

--- vue_laravel_backend/src/views/Dashboard.vue
+++ vue_laravel_backend/src/views/Dashboard.vue
@@ -1,10 +1,14 @@
-<!-- <script setup>
-const Dashboard={
-    name: "Dashboard"
-};
-</script> -->
+<script setup>
+import NavBar from '../components/NavBar.vue'
+import { useStore } from '@pinia/vue';
+
+const store = useStore();
+const token = store.getTestToken()
+</script>
 <template>
     <h1>Dashboard</h1>
+    <NavBar/>
+    <p>Token: {{ token }}</p>
 </template>
 
 
